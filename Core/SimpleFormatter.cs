dc0fce6cf1ca7b3e6c7a06f6d17a1030d3906a98
Adrian Banks <github@adrianbanks.co.uk>
2012-06-14 15:57:15 +0100

    move the formatter into a separate class and add some tests

---
+++ Core/SimpleFormatter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DomainModels;
+
+namespace Core
+{
+    public class SimpleFormatter
+    {
+        public const string header = 
+@"Programmer          Skills              Recommends
+----------          ------              ----------\r\n";
+            
+
+        public string Format(IEnumerable<Programmer> programmers)
+        {
+            var report = new StringBuilder();
+
+            report.Append(header);
+
+            foreach (var programmer in programmers.OrderBy(p => p.Name))
+            {
+                var skills = string.Join(", ", programmer.Skills);
+                var recommendations = string.Join(", ", programmer.Recommendations.Select(p => p.Name));
+                report.AppendFormat("{0,-20}{1,-20}{2,-20}{3}", programmer.Name, skills, recommendations, Environment.NewLine);
+            }
+
+            return report.ToString();
+        }
+    }
+}
