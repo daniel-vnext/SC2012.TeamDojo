e71dee647bf62a457c94ce3c743764614d27a010
Adrian Banks <github@adrianbanks.co.uk>
2012-06-14 15:22:29 +0100

    acceptance test 1 done with dummy data

---
+++ AcceptanceTestOne/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DomainModels;
+
+namespace AcceptanceTestOne
+{
+    class Program
+    {
+        static void Main()
+        {
+            //TODO: replace this test data with the real data
+            var ben = new Programmer("Ben");
+            ben.Skills.Add(Skills.CSharp);
+            ben.Skills.Add(Skills.Java);
+            var adrian = new Programmer("Adrian");
+            adrian.Skills.Add(Skills.CSharp);
+            adrian.Skills.Add(Skills.Ruby);
+
+            ben.Recommendations.Add(adrian);
+
+            var programmers = new List<Programmer>()
+                                  {
+                                              adrian,
+                                              ben
+                                  };
+
+            foreach (var programmer in programmers.OrderBy(p => p.Name))
+            {
+                var skills = string.Join(", ", programmer.Skills);
+                var recommendations = string.Join(", ", programmer.Recommendations.Select(p => p.Name));
+                Console.WriteLine("{0,-20}{1,-20}{2,-20}", programmer.Name, skills, recommendations);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
