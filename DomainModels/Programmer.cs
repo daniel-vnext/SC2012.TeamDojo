27d5756aa793df5d18a7340fa03d8b1b69a4a206
Alastair Smith <github@alastairsmith.me.uk>
2012-06-14 15:01:57 +0100

    Add Programmer domain model

---
+++ DomainModels/Programmer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DomainModels
+{
+    public class Programmer
+    {
+        public string Name { get; private set; }
+        public ICollection<Programmer> Recommendations { get; private set; }
+        public ICollection<Skills> Skills { get; private set; }
+
+        public Programmer(string name)
+        {
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("name");
+            }
+
+            Name = name;
+            Recommendations = new List<Programmer>();
+            Skills = new List<Skills>();
+        }
+    }
+}
