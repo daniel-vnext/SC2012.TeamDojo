dd9622ca8f7e0f6ea9d82519f67d4a1ef02a4354
Dan Burnett <burnett.daniel@gmail.com>
2012-06-14 14:52:21 +0100

    add a test to check TC is working ok

---
+++ Core.Tests/DummyTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Core.Tests
+{
+    [TestFixture]
+    public class DummyTest
+    {
+        [Test]
+        public void TeamCity_should_run_this_test()
+        {
+            Console.WriteLine("Hi, this line should appear in the TC logs! :)");
+        }
+    }
+}
